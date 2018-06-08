6dd42c72b3b3a4347bf80189058762fd45d8322e
Akshith Kandakatla <akshithrk@users.noreply.github.com>
2018-06-08 09:34:58 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -39,7 +39,7 @@
           var active = patient.active;
 //           console.log(active);
 //           var add = patient.address;
-          var address = patient.address.city //+ ', ' + add.address[0].line;
+          var address = patient.address[0].city //+ ', ' + add.address[0].line;
           
 //           console.log(address);
           var phone = patient.telecom.value;
